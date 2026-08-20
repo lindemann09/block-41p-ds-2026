bf18f69b00ec41f8e0b188913394a7b66e7814fd
Oliver Lindemann <lindemann09@googlemail.com>
2026-08-20 15:11:50 +0200

    Update utils.R

--- data/utils.R
+++ data/utils.R
@@ -1,6 +1,5 @@
-COURSE_REPO = "lindemann09/block-41p-ds-2026"
-
 download_course_data <- function(data_name) {
+  COURSE_REPO = "lindemann09/block-41p-ds-2026"
   file = paste0("https://raw.githubusercontent.com/", COURSE_REPO,
                 "/refs/heads/main/data/", data_name, ".csv")
   return(read.csv(file))
