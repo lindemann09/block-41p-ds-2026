fa9029879a94f106ef8c64c848f0be887ecbc43c
Oliver Lindemann <lindemann09@googlemail.com>
2026-08-24 12:53:35 +0200

    update manual

--- manual/incl/useful_functions.R
+++
@@ -1,8 +0,0 @@
-
-week <- function(x) {
-  w = as.Date('21.4.2025',format='%d.%m.%Y') + 7*(x-1)
-  txt = format(w, format = "%d/%m")
-  return(txt)
-}
-
-canvas <- "[Canvas](https://canvas.eur.nl/courses/XXX)" # <------- CHANGE URL HERE
